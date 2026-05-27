770e35fce6876de2a1a90fdb3a84f8ce62667362
Emily Nguyen <emn008@ucsd.edu>
2026-05-26 17:26:08 -0700

    explore started

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Lab8-Starter
+
+In your README.md, please add a short paragraph answering the question: How are graceful degradation and service workers related? (Answer this after doing the part below)
+
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -44,16 +44,20 @@ function initializeServiceWorker() {
   /*******************/
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
-  // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  // B1
+  if ('serviceWorker' in navigator) {
+
+    // B2
+    window.addEventListener('load', () => {
+
+      // B3
+      navigator.serviceWorker.register('./sw.js')
+        // B4
+        .then(() => console.log('Service worker registered successfully'))
+        // B5
+        .catch(() => console.log('Service worker registration failed'));
+    });
+  }
 }
 
 /**
@@ -72,7 +76,7 @@ async function getRecipes() {
 
   // A2
   let recipes = [];
-  
+
   // A3-11
   return new Promise(async (resolve, reject) => {
     for(let i = 0; i < RECIPE_URLS.length; i++) {

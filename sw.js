d544541deab685e9f464e6fa4de4f54f1be84b02
Emily Nguyen <emn008@ucsd.edu>
2026-05-26 17:50:34 -0700

    recipe links changed

--- sw.js
+++ sw.js
@@ -9,12 +9,12 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6 
       return cache.addAll([
-      'https://emilyynguyen.github.io/Lab8_Starter/recipes/1_50-thanksgiving-side-dishes.json',
-      'https://emilyynguyen.github.io/Lab8_Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
-      'https://emilyynguyen.github.io/Lab8_Starter/recipes/3_moms-cornbread-stuffing.json',
-      'https://emilyynguyen.github.io/Lab8_Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
-      'https://emilyynguyen.github.io/Lab8_Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
-      'https://emilyynguyen.github.io/Lab8_Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
       ]);
     })
   );
